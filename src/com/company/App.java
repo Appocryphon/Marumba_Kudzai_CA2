3c96c4b9bf4e513d14d9aa74326e1c8d89283cff
Appocryphon <kudzaimarumba1@gmail.com>
2022-03-17 11:57:57 +0000

    Minor fixes to hashMap and ArrayList alongside the menu

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.util.*;
 
 public class App {
-//
-   mangaManager mangaManager;
-    ArrayList<Manga> mangaList;
+    //
+    mangaManager mangaManager;
+    List<Manga> mangaList;
+
     public static void main(String[] args) {
         App app = new App();
         app.start();
@@ -16,24 +17,23 @@ public class App {
 
     private void start() {
         this.mangaList = new ArrayList<>();
-        mangaList.add(new Manga("Date A Live",2013, 2012));
+        mangaList.add(new Manga("Date A Live", 2013, 2012));
         mangaList.add(new Manga("Goblin Slayer", 2018, 2016));
         mangaList.add(new Manga("Jobless Reincarnation", 2021, 2014));
         mangaList.add(new Manga("Shingeki no Kyojin", 2013, 2009));
-        mangaList.add(new Manga("Dragon Ball Z",2009, 2006));
-        mangaList.add(new Manga("Fairy Tail",2013, 2012));
-        mangaList.add(new Manga("Bleach",2004, 2001));
-        mangaList.add(new Manga("Kimetsu no Yaiba",2019, 2016));
-        mangaList.add(new Manga("Tokyo Ghoul",2014, 2011));
-        mangaList.add(new Manga("Akame ga Kill",2014, 2010));
+        mangaList.add(new Manga("Dragon Ball Z", 2009, 2006));
+        mangaList.add(new Manga("Fairy Tail", 2013, 2012));
+        mangaList.add(new Manga("Bleach", 2004, 2001));
+        mangaList.add(new Manga("Kimetsu no Yaiba", 2019, 2016));
+        mangaList.add(new Manga("Tokyo Ghoul", 2014, 2011));
+        mangaList.add(new Manga("Akame ga Kill", 2014, 2010));
 
-////
 
-        try
-        {
+
+
+        try {
             displayMainMenu();        // User Interface - Menu
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -42,8 +42,7 @@ public class App {
     }
 
 
-    private void displayMainMenu() throws IOException
-    {
+    private void displayMainMenu() throws IOException {
 
         final String MENU_ITEMS = "\n*** MAIN MENU OF OPTIONS ***\n"
                 + "1. ArrayList\n"
@@ -59,15 +58,12 @@ public class App {
 
         Scanner keyboard = new Scanner(System.in);
         int option = 0;
-        do
-        {
+        do {
             System.out.println("\n" + MENU_ITEMS);
-            try
-            {
+            try {
                 String usersInput = keyboard.nextLine();
                 option = Integer.parseInt(usersInput);
-                switch (option)
-                {
+                switch (option) {
                     case Arrays:
                         System.out.println("Display all Manga available:");
                         displayAllManga();
@@ -89,8 +85,7 @@ public class App {
                         break;
                 }
 
-            } catch (InputMismatchException | NumberFormatException e)
-            {
+            } catch (InputMismatchException | NumberFormatException e) {
                 System.out.print("Invalid option - please enter number in range");
             }
         } while (option != EXIT);
@@ -100,48 +95,51 @@ public class App {
     }
 
     public void displayAllManga() {
-        for(Manga m : this.mangaList)
-        {
+        for (Manga m : this.mangaList) {
             System.out.println(m.toString());
         }
     }
 
 
-    public void mangaHashMap()
-    {
-        HashMap<Integer, Manga> hashMap = new HashMap<>();
-
-        hashMap.put(0, new Manga("Date A Live",2013, 2012));
-        hashMap.put(1, new Manga("Goblin Slayer", 2018, 2016));
-        hashMap.put(2, new Manga("Jobless Reincarnation", 2021, 2014));
-        hashMap.put(3, new Manga("Shingeki no Kyojin", 2013, 2009));
-        hashMap.put(4, new Manga("Dragon Ball Z",2009, 2006));
-        hashMap.put(5, new Manga("Fairy Tail",2013, 2012));
-        hashMap.put(6, new Manga("Bleach",2004, 2001));
-        hashMap.put(7, new Manga("Kimetsu no Yaiba",2019, 2016));
-        hashMap.put(8, new Manga("Tokyo Ghoul",2014, 2011));
-        hashMap.put(9, new Manga("Akame ga Kill",2014, 2010));
+    public void mangaHashMap() {
 
 
-        int key = 0;
-        Manga favourite = hashMap.get(key);
-    System.out.println("Please enter a key: ");
-    Scanner sc = new Scanner(System.in);
-    key = sc.nextInt();
 
-     while (key >= 0 || key <= 9)
-     {
-         if (key >= 0 || key <= 9)
+
+        Map<String, Manga> mangaMap = new HashMap<>();
+
+        mangaMap.put("1",new Manga("Date A Live", 2013, 2012));
+        mangaMap.put("2", new Manga("Goblin Slayer", 2018, 2016));
+        mangaMap.put("3", new Manga("Jobless Reincarnation", 2021, 2014));
+        mangaMap.put("4", new Manga("Shingeki no Kyojin", 2013, 2009));
+        mangaMap.put("5", new Manga("Dragon Ball Z", 2009, 2006));
+        mangaMap.put("6", new Manga("Fairy Tail", 2013, 2012));
+        mangaMap.put("7", new Manga("Bleach", 2004, 2001));
+        mangaMap.put("8", new Manga("Kimetsu no Yaiba", 2019, 2016));
+        mangaMap.put("9", new Manga("Tokyo Ghoul", 2014, 2011));
+        mangaMap.put("10", new Manga("Akame ga Kill", 2014, 2010));
+////
+
+
+         String key="";
+         System.out.println("Please enter a key: ");
+         Scanner sc = new Scanner(System.in);
+         key = sc.next();
+
+
+         if (mangaMap.containsKey(key))
          {
-             System.out.println(favourite + " Is your favourite Manga");
+             System.out.println("mangaMap contains key" + key);
+             System.out.println();
+
          }
          else
          {
              System.out.println("The entered key does not match any in the Map, please try again.");
-             key = sc.nextInt();
+             key = sc.nextLine();
          }
 
-     }
+
 
 
 
